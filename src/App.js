b34ecbc5fca83f4a66adc99015f3b1021799c2a4
MinosFenn <45685449+MinosFenn@users.noreply.github.com>
2022-09-25 19:47:34 +0200

    sunrise sunset, and images added

--- src/App.js
+++ src/App.js
@@ -1,121 +1,169 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import technicJacket from './images/clothes/doudoune.png';
+import softPant from './images/clothes/pantalon-leger.png';
+import technicPant from './images/clothes/pantalong-technique.png';
+import polo from './images/clothes/polo.png';
+import pull from './images/clothes/pullover.png';
+import short from './images/clothes/short.png';
+import sweat from './images/clothes/sweat.png';
+import tshirt from './images/clothes/Tshirt.png';
+import softJacket from './images/clothes/veste-legere.png';
+import longJacket from './images/clothes/veste-longue.png';
+import middleJacket from './images/clothes/veste-moyenne.png';
+import jeans from './images/clothes/jean.png';
+import sleevelessJacket from './images/clothes/doudoune-sans-manche.png';
+import shirt from './images/clothes/doudoune-sans-manche.png';
+import sunrise from './images/sunrise-weather-symbol_icon-icons.com_64261.png';
+import sunset from './images/sunset-fill-interface-symbol_icon-icons.com_64262.png';
 
 const api = {
-  base: "https://api.openweathermap.org/data/2.5/",
+  base: 'https://api.openweathermap.org/data/2.5/',
   // translate city into lat long with google maps api
   // new url: https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 };
 
 function App() {
-  const [query, setQuery] = useState("");
-  const [weather, setWeather] = useState("");
-  const [indicator, setMainIndic] = useState("");
-  const [rain, setRain] = useState("");
-  const [snow, setSnow] = useState("");
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState('');
+  const [indicator, setMainIndic] = useState('');
+
   const search = (evt) => {
-    if (evt.key === "Enter") {
-      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${process.env.REACT_APP_WEATHER_API_KEY}`)
+    if (evt.key === 'Enter') {
+      fetch(
+        `${api.base}weather?q=${query}&units=metric&APPID=${process.env.REACT_APP_WEATHER_API_KEY}`
+      )
         .then((res) => res.json())
         .then((result) => {
           setWeather(result);
-          setQuery("");
+          setQuery('');
           setMainIndic(result.main);
-          setRain(result.rain);
-          setSnow(result.snow);
           console.log(result);
-        });
+        })
+        .catch((err) => console.log(err.type));
     }
   };
+  // console.log(indicator.temp);
 
-  console.log(weather.main);
+  // console.log(weather.weather[0].main);
+  // console.log(Object.values(weather.weather[0]));
 
   const dateBuilder = (d) => {
     let months = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
     ];
     let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
     ];
 
     let day = days[d.getDay()];
     let date = d.getDate();
     let month = months[d.getMonth()];
     let year = d.getFullYear();
-
     return `${day} ${date} ${month} ${year}`;
   };
+
+
+  const SecondsToHms = (props) => {
+    let date = Date(`${props.value}`);
+    const moment = props.value * 1000;
+    const dateObject = new Date(moment);
+    const humanDateFormat = dateObject.toLocaleString() //2019-12-9 10:30:15
+    const hour = dateObject.toLocaleString("fr-FR", {hour: "numeric"}) // 10 AM
+const minute= dateObject.toLocaleString("fr-FR", {minute: "numeric"}) // 30
+return `${hour} ${minute}`
+
+console.log(moment)
+    console.log(props.value)
+
+   
+    let hours = 1;
+    let minutes = 2;
+    let seconds = 3;
+    return `${hours} ${minutes} ${seconds}`;
+}
+  // create feature holiday: choose a destination, bag capacity, time period > recommendation system
+  // switch replace by object map at some point
   function SwitchCaseTempTop(props) {
     console.log(props.value);
     switch (true) {
-      case props.value <= 20:
-        return "T-shirt plus hoodie";
-      case props.value <= 30 && props.value >= 20:
-        return `a t-shirt is enough`;
-      case props.value >= 30:
-        return `you can go naked it's warm enough`;
-      default:
-        return "";
-    }
-  }
-  function SwitchCaseTopJacket(props) {
-    console.log(props.value);
-    switch (true) {
-      case props.value <= 20:
-        return "T-shirt plus hoodie";
-      case props.value <= 30 && props.value >= 20:
-        return `a t-shirt is enough`;
-      case props.value >= 30:
-        return `you can go naked it's warm enough`;
-      default:
-        return "";
-    }
-  }
-  function SwitchCaseElseRain(props) {
-    console.log(props.value);
-    switch (true) {
-      case props.value <= 20:
-        return "T-shirt plus hoodie";
-      case props.value <= 30 && props.value >= 20:
-        return `a t-shirt is enough`;
-      case props.value >= 30:
-        return `you can go naked it's warm enough`;
-      default:
-        return "";
-    }
-  }
+      //Snow cases
+      case props.value <= 0:
+        return (
+          <>
+            <img src={technicJacket} />
+            <img src={technicPant} />
+            <img src={tshirt} />
+          </>
+        );
+      case weather.weather[0].main && props.value <= 10:
+        return (
+          <>
+            <img src={softJacket} />
+            <img src={jeans} />
+            <img src={tshirt} />
+          </>
+        );
+      case weather.weather[0].main && props.value <= -20:
+        return (
+          <>
+            <img src={technicJacket} />
+            <img src={pull} />
+            <img src={tshirt} />
+            <img src={technicPant} />
+          </>
+        );
+      //Rain cases
 
-  function SwitchCaseElseSnow(props) {
-    console.log(props.value);
-    switch (true) {
+      // Warm cases
       case props.value <= 20:
-        return "T-shirt plus hoodie";
-      case props.value >= 20:
-        return `a t-shirt is enough`;
+        return (
+          <>
+            <img src={middleJacket} />
+            <img src={tshirt} />
+            <img src={jeans} />
+          </>
+        );
+      case props.value <= 30 && props.value >= 20:
+        return (
+          <>
+            <img src={tshirt} />
+            <img src={jeans} />
+          </>
+        );
       case props.value >= 30:
-        return `you can go naked it's warm enough`;
+        return (
+          <>
+            <img src={tshirt} />
+            <img src={short} />
+          </>
+        );
       default:
-        return "";
+        return '404';
     }
   }
   return (
-    <div className={(typeof weather.main != "undefined") ? (weather.weather[0].main) : 'app'}>
+    <div
+      className={
+        typeof weather.main != 'undefined' ? weather.weather[0].main : 'app'
+      }
+    >
       <main>
         <div className="search-box">
           <input
@@ -127,23 +175,33 @@ function App() {
             onKeyPress={search}
           ></input>
         </div>
-        {typeof weather.main !== "undefined" ? (
+        {typeof weather.main !== 'undefined' ? (
           <div>
             <div className="location-box">
               <div className="location">
                 {weather.name}, {weather.sys.country}
               </div>
+             
+
               <div className="date">{dateBuilder(new Date())}</div>
             </div>
             <div className="weather-box">
               <div className="top">
                 <SwitchCaseTempTop value={indicator.temp} />
+                {/* fix value to test switch case  */}
+                {/* <SwitchCaseTempTop value="0" /> */}
               </div>
               <div className="temp">{Math.round(weather.main.temp)}°C</div>
-              <div className="weather">{weather.weather[0].main}</div>
+              <div className="location">
+                {' '}
+                <img src={sunrise} />
+                <SecondsToHms value={weather.sys.sunrise}/>
+                {/* {SecondsToHms( Date(`${weather.sys.sunrise}`))} */}
 
-              <SwitchCaseElseRain value={rain} />
-              <SwitchCaseElseSnow value={snow} />
+                <img src={sunset} />
+                <SecondsToHms value={weather.sys.sunset}/>
+              </div>
+              <div className="weather">{weather.weather[0].main}</div>
             </div>
           </div>
         ) : (
